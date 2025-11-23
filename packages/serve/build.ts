a33daca805dac05b6567baf9852440beb179642c
tomfln <tomfln@gmx.de>
2025-11-23 20:05:11 +0100

    simplify serve build

--- packages/serve/build.ts
+++ packages/serve/build.ts
@@ -1,30 +1,8 @@
 import { buildSubmodule, buildRust } from '@csm/core'
-import { join } from 'path'
+import { $ } from 'bun'
 
 await buildSubmodule(async () => {
-  const appDir = join(process.cwd(), 'src', 'app')
-  
-  console.log('Installing frontend dependencies...')
-  const install = Bun.spawn(['pnpm', 'install'], {
-    cwd: appDir,
-    stdin: 'inherit',
-    stdout: 'inherit',
-    stderr: 'inherit',
-  })
-  if ((await install.exited) !== 0) {
-    throw new Error('Frontend install failed')
-  }
-
-  console.log('Building frontend...')
-  const build = Bun.spawn(['pnpm', 'build'], {
-    cwd: appDir,
-    stdin: 'inherit',
-    stdout: 'inherit',
-    stderr: 'inherit',
-  })
-  if ((await build.exited) !== 0) {
-    throw new Error('Frontend build failed')
-  }
+  await $`cd src/app && pnpm i && pnpm build`
 
   return buildRust('src', 'serve')
 })
